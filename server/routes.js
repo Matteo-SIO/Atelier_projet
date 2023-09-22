832a81e8c433d140b0466b9cc412b15d3549b4e1
users <mattbacq2004@mail.com>
2023-09-22 13:32:38 +0200

    Added TypeMaterials route

--- server/routes.js
+++ server/routes.js
@@ -2,4 +2,5 @@ export let defineRoutes = async (server) => {
     (await import('./routes/AuthRoute.js')).default(server, '/api/auth');
     (await import('./routes/MaterialsRoute.js')).default(server, '/api/materials');
     (await import('./routes/UsersRoute.js')).default(server, '/api/users');
+    (await import('./routes/TypeMaterialsRoute.js')).default(server, '/api/type-materials');
 }
---
+++ server/routes/TypeMaterialsRoute.js
@@ -0,0 +1,90 @@
+import {verifyToken} from "../../utils/SessionUtil.js";
+import Tables from "../../database/Tables.js";
+
+export default (server, BASE_PATH) => {
+
+    // List all the types materials
+    server.get(BASE_PATH + '/', async (request, reply) => {
+        let token = request.headers.authorization;
+        if (!verifyToken(token)) {
+            reply.code(401);
+            reply.send({error: 'Unauthorized'});
+            return;
+        }
+
+        let typeMaterials = await Tables.TypeMaterial.findAll({});
+
+        reply.code(200);
+        reply.send(typeMaterials);
+    });
+
+    // Get a type material by id
+    server.get(BASE_PATH + '/:id', async (request, reply) => {
+        let token = request.headers.authorization;
+        if (!verifyToken(token)) {
+            reply.code(401);
+            reply.send({error: 'Unauthorized'});
+            return;
+        }
+
+        let typeMaterial = await Tables.TypeMaterial.findOne({
+            where: {
+                id: request.params.id
+            }
+        });
+
+        if (!typeMaterial) {
+            reply.code(404);
+            reply.send({error: 'Not Found'});
+            return;
+        }
+
+        reply.code(200);
+        reply.send(typeMaterial);
+    });
+
+    // Create a type material
+    // require role 'manager'
+    server.post(BASE_PATH + '/', async (request, reply) => {
+        let token = request.headers.authorization;
+        let decodedToken = verifyToken(token);
+        if (!decodedToken || decodedToken.role !== 'manager') {
+            reply.code(401);
+            reply.send({error: 'Unauthorized'});
+            return;
+        }
+
+        try {
+            await Tables.TypeMaterial.create(request.body);
+            reply.code(201);
+        } catch (error) {
+            reply.code(400);
+            reply.send({
+                error: error.errors[0].message
+            });
+            return;
+        }
+    });
+
+    // Update a type material by id
+    // Require an authorization with the role 'manager'
+    server.put(BASE_PATH + '/:id', async (request, reply) => {
+        let token = request.headers.authorization;
+        let decodedToken = verifyToken(token);
+        if (!decodedToken || decodedToken.role !== 'manager') {
+            reply.code(401);
+            reply.send({error: 'Unauthorized'});
+            return;
+        }
+
+        await Tables.TypeMaterial.update(request.body, {
+            where: {
+                id: request.params.id
+            }
+        });
+
+        reply.code(200);
+        reply.send({success: true});
+    });
+
+}
