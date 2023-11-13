fee3f6fa04957801d701a0edebdbc4ca2f8a62c9
users <mattbacq2004@mail.com>
2023-11-13 13:17:06 +0100

    Adding item list

--- web/src/routes/materiels/+page.svelte
+++ web/src/routes/materiels/+page.svelte
@@ -22,59 +22,69 @@
                 Liste des Matériels Informatiques
             </div>
             <ul class="list-group list-group-flush">
-                <!-- Produit 1 -->
-                <li class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <h5 class="mb-0">
-                                <span class="badge bg-info me-2">Ordinateur</span>
-                                Nom du Produit 1
-                            </h5>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="d-flex align-items-center">
-                                <span class="badge bg-success me-2">Disponible</span>
-                                <small class="text-muted me-2">Disponible le 01/01/2023</small>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
+
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Nom du Produit</th>
+                        <th>Statut</th>
+                        <th>Date</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <!-- Produit 1 -->
+                    <tr>
+                        <td>
+                            <span class="badge bg-info">Ordinateur</span>
+                        </td>
+                        <td>Nom du Produit 1</td>
+                        <td>
+                            <span class="badge bg-success">Disponible</span>
+                        </td>
+                        <td>Disponible le 01/01/2023</td>
+                        <td>
                             <div class="btn-group">
                                 <button type="button" class="btn btn-sm btn-primary">Réserver</button>
                                 <button type="button" class="btn btn-sm btn-info">Info</button>
                                 <button type="button" class="btn btn-sm btn-warning">Modifier</button>
                                 <button type="button" class="btn btn-sm btn-danger">Archiver</button>
                             </div>
-                        </div>
-                    </div>
-                </li>
+                        </td>
+                    </tr>
 
-                <!-- Produit 2 -->
-                <li class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <h5 class="mb-0">
-                                <span class="badge bg-warning me-2">Souris</span>
-                                Nom du Produit 2
-                            </h5>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="d-flex align-items-center">
-                                <span class="badge bg-danger me-2">Emprunté</span>
-                                <small class="text-muted me-2">Disponible le 02/01/2023</small>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
+                    <!-- Produit 2 -->
+                    <tr>
+                        <td>
+                            <span class="badge bg-warning">Souris</span>
+                        </td>
+                        <td>Nom du Produit 2</td>
+                        <td>
+                            <span class="badge bg-danger">Emprunté</span>
+                        </td>
+                        <td>Disponible le 02/01/2023</td>
+                        <td>
                             <div class="btn-group">
                                 <button type="button" class="btn btn-sm btn-info">Info</button>
                                 <button type="button" class="btn btn-sm btn-warning">Modifier</button>
                             </div>
-                        </div>
-                    </div>
-                </li>
+                        </td>
+                    </tr>
 
-                <!-- Ajoutez d'autres produits au besoin -->
+                    <!-- Ajoutez d'autres produits au besoin -->
 
-            </ul>
+                    </tbody>
+                </table>
+
+
+
+
+
+
+
+
+    </ul>
         </div>
     </div>
 
