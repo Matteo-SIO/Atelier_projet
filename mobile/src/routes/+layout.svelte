eedcd2fe1961861b2f4a6a5d5b0eb8c7432ddb8d
users <mattbacq2004@mail.com>
2023-12-15 11:49:45 +0100

    Created mobile app

---
+++ mobile/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { setupIonicBase } from 'ionic-svelte';
+
+    /* load and register all components - you can also import separately to code split */
+    import 'ionic-svelte/components/all';
+
+    /* run base configuration code from ionic/core */
+    setupIonicBase();
+</script>
+
+<style lang="scss">
+  @import "../../static/common.scss";
+</style>
+
+<ion-app>
+    <slot />
+</ion-app>
