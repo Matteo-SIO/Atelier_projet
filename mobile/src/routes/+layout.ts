9c626408bc619001d15ea3492f0112c3ec32f37a
users <mattbacq2004@mail.com>
2023-12-24 02:29:45 +0100

    Setup mobile + copy

--- mobile/src/routes/+layout.ts
+++ mobile/src/routes/+layout.ts
@@ -2,6 +2,8 @@ import {redirectIfNotLoggedIn} from "$lib/Session.ts";
 import {redirect} from "@sveltejs/kit";
 import {browser} from "$app/environment";
 
+export const prerender = true;
+
 import { page } from '$app/stores';
 import {get} from "svelte/store";
 
